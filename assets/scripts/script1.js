67d95f89e7e1ac839171a5f66cf8ac6d601afc47
anjumzaki <anjumzaki8@gmail.com>
2019-02-25 01:11:33 +0500

    asdsa

---
+++ assets/scripts/script1.js
@@ -0,0 +1,42 @@
+
+
+function openNav() {
+  $('.carousel').carousel('pause');
+  document.getElementById('overlay').classList.add('fadeMe');
+  if (window.matchMedia("(min-width: 1101px)").matches) {
+    document.getElementById("mySidenav").style.width = "50%";
+  } else {
+    document.getElementById("mySidenav").style.width = "100%"
+}
+}
+  $(window).scroll(function() {
+  var $height = $(window).scrollTop();
+if($height < 100) {
+  $('.carousel').carousel('cycle');
+  } else {
+  $('.carousel').carousel('pause');
+  }
+});
+
+function openDNav() {
+        document.getElementById('d-nav').classList.add('dis-none');
+        document.getElementById('s-nav').classList.remove('dis-none');
+      }
+    function closeDNav(){
+        document.getElementById('d-nav').classList.remove('dis-none');
+        document.getElementById('s-nav').classList.add('dis-none')
+    } 
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  $('.carousel').carousel('cycle');
+  document.getElementById('overlay').classList.remove('fadeMe');
+
+
+  }
+  $(document).ready(function () {           
+          $('.carousel').carousel();    
+          $('.carousel').carousel({
+interval: 1500
+})   
+      });
+     
