c7e04b7da18b223c3ecdcaf0af254346339a4892
anjumzaki <anjumzaki8@gmail.com>
2019-02-28 19:18:34 +0500

    destination pages turkey

--- assets/scripts/script1.js
+++ assets/scripts/script1.js
@@ -7,50 +7,74 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "50%";
   } else {
     document.getElementById("mySidenav").style.width = "100%"
+  }
 }
-}
-  $(window).scroll(function() {
+$(window).scroll(function () {
   var $height = $(window).scrollTop();
-if($height < 100) {
-  $('.carousel').carousel('cycle');
+  if ($height < 100) {
+    $('.carousel').carousel('cycle');
   } else {
-  $('.carousel').carousel('pause');
+    $('.carousel').carousel('pause');
   }
 });
 
 function openDNav() {
-        document.getElementById('d-nav').classList.add('dis-none');
-        document.getElementById('s-nav').classList.remove('dis-none');
-      }
-    function closeDNav(){
-        document.getElementById('d-nav').classList.remove('dis-none');
-        document.getElementById('s-nav').classList.add('dis-none')
-    } 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('d-nav').classList.add('dis-none');
+  document.getElementById('s-nav').classList.remove('dis-none');
+}
+function closeDNav() {
+  document.getElementById('d-nav').classList.remove('dis-none');
+  document.getElementById('s-nav').classList.add('dis-none')
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
   $('.carousel').carousel('cycle');
   document.getElementById('overlay').classList.remove('fadeMe');
 
 
+}
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $('.carousel').carousel({
+    interval: 1500
+  })
+});
+var places;
+var imgn;
+function showds(img, place) {
+  imgn = parseInt(img)
+  if (place == 'istanbul') {
+    places = [
+      'assets/styles/image/tr/ist/grand-bazaar-istanbul-turkey.jpg',
+      'assets/styles/image/tr/ist/Hagia.Sophia.jpg',
+      'assets/styles/image/tr/ist/istanbul-Dolmabahce-sarayi.jpg',
+      'assets/styles/image/tr/ist/istanbul-modern-museum.jpg',
+      'assets/styles/image/tr/ist/Kiz-Kulesi-Istanbul-Turkey.jpg',
+    ]
   }
-  $(document).ready(function () {           
-          $('.carousel').carousel();    
-          $('.carousel').carousel({
-interval: 1500
-})   
-      });
-      // var width1 = $(window).width();
-      // var hotelsimgs = document.getElementById('hotels12');
-      // var hotelDiv = document.getElementById('with-hotel');
-      // $(window).resize(function() {
-      //   var width = $(window).width();
-      //   var height = $(window).height();
-      
-      //   if(width<=640 || width1<=640) {
-      //     hotelsimgs.remove();
-      //   }
-      //   else{
-      //     document.getElementById('for-append').innerHTML(hotelsimgs)
-      //   }
-      
-      // })
+  document.getElementById('new-modes').innerHTML = '<img ' + 'src= ' + '"' + places[imgn] + '"' + '/>'
+  return imgn
+}
+
+function incre() {
+  if (imgn >= 4) {
+    imgn = 0;
+    showds(imgn)
+  }
+  else {
+    imgn++;
+    showds(imgn)
+  }
+}
+
+function decre() {
+  if (imgn <= 0) {
+    imgn = 4;
+    showds(imgn)
+  }
+  else {
+    imgn--;
+    showds(imgn)
+  }
+}
+
